8032fe9dabadf77c9c427328ffb966aaf2b1fae0
DevelopJKong <ybaram40@gmail.com>
2022-01-31 17:31:11 +0900

    packagist 업로드 전 commit

--- src/Application.php
+++ src/Application.php
@@ -1,21 +1,34 @@
 <?php
 
-namespace php_oop_board;
+namespace Eclair;
 
-use php_oop_board\Support\ServiceProvider;
+use Eclair\Support\ServiceProvider;
 
 class Application
 {
+    /**
+     * @var ServiceProvider[] $providers
+     */
     private $providers = [];
 
+    /**
+     * Create a App Instance
+     *
+     * @param ServiceProvider[] $providers
+     *
+     * @return Application
+     */
     public function __construct($providers = [])
     {
         $this->providers = $providers;
-        array_walk($this->provider,fn($provider) => $provider::register());
+        array_walk($this->providers, fn ($provider) => $provider::register());
     }
 
+    /**
+     * Run Application
+     */
     public function boot()
     {
-        array_walk($this->providers,fn($provider) => $provider::boot());
+        array_walk($this->providers, fn ($provider) => $provider::boot());
     }
 }
